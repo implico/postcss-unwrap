9cc6d056c40cd64b42afba2e0f8166fee98e1c97
Bartosz Sak <bartosz.sak@gmail.com>
2019-07-17 14:46:49 +0200

    tests: extract readFile to helper

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
 import postcss from 'postcss';
+
+export const readFile = file => fs.readFileSync(file, 'utf-8');
 
 export const plugify = callback => postcss.plugin('postcss-unwrap', () => callback);
--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
-import fs from 'fs';
 import postcss from 'postcss';
 import test from 'ava';
-import {plugify} from './helpers';
+import {readFile, plugify} from './helpers';
 import unwrap from '..';
 
 test.before(t => {
-  t.context.CSS = fs.readFileSync('./test/fixtures/test.css', 'utf-8');
-  t.context.CSS_UNWRAPPED = fs.readFileSync('./test/fixtures/test-unwrapped.css', 'utf-8');
+  t.context.CSS = readFile('./test/fixtures/test.css');
+  t.context.CSS_UNWRAPPED = readFile('./test/fixtures/test-unwrapped.css');
 });
 
 test('main', async t => {
